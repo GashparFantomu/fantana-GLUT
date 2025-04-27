6aea8b02c90851bc6613ea40ff4f3a158ccb7ff5
GashparFantomu <128855619+GashparFantomu@users.noreply.github.com>
2025-04-27 18:40:03 +0300

    solved a roof bug

--- fantana/mainFantana.cpp
+++ fantana/mainFantana.cpp
@@ -324,14 +324,15 @@ void roofWell() {
     glVertex3f(-1.0, 2.2, -1.2);
     glEnd();
 
-
-    glBegin(GL_QUAD_STRIP);
+    glBegin(GL_QUADS);
     glColor3f(0.0, 1.0, 0.0);
-    glVertex3f(-1.0, 2.1, -1.2);
-    glVertex3f(-1.0, 2.1, 1.2);
-    glVertex3f(-1.0, 2.2, 1.2);
-    glVertex3f(-1.0, 2.2, -1.2);
+    glVertex3f(1.0, 2.1, -1.2);
+    glVertex3f(1.0, 2.1, 1.2);
+    glVertex3f(1.0, 2.2, 1.2);
+    glVertex3f(1.0, 2.2, -1.2);
+    
     glEnd();
+
 }
 
 void barBucketWell() {
